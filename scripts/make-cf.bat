fc75be4804a64c1113f90bac9934e0147625efa5
Stepa86 <Stepa86@gmail.com>
2018-07-13 19:02:56 +0300

    Доработка скрипта для прекоммита

--- scripts/make-cf.bat
+++ scripts/make-cf.bat
@@ -1,6 +1,10 @@
-rd /S /Q config
-md config
-rd /S /Q workspace
-md workspace
-call ring edt workspace export --project ../eye-prophet-cf --configuration-files ./config --workspace-location ./workspace
-call runner compile -s config -o ./cf/1Cv8.cf
+set WORKSPACE=%~dp0workspace
+set SRC=%~dp0src
+set EDT_PROJECT=%~dp0..\eye-prophet-cf
+set CF=%~dp0..\cf\1Cv8.cf
+rd /S /Q %SRC%
+md %SRC%
+rd /S /Q %WORKSPACE%
+md %WORKSPACE%
+call ring edt workspace export --project %EDT_PROJECT% --configuration-files %SRC% --workspace-location %WORKSPACE%
+call runner compile -s %SRC% -o %CF%
